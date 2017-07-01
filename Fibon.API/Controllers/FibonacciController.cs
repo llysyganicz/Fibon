6286fdc804e4a06aedbe829686ad3a7ae43807fb
llysyganicz <llysyganicz@gmail.com>
2017-07-01 10:01:49 +0200

    Added Fibonacci controller

---
+++ Fibon.API/Controllers/FibonacciController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.API.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            return Content("0");
+        }
+
+        [HttpPost("{number}")]
+        public IActionResult Post(int number)
+        {
+            return Accepted($"Fibonacci/{number}", null);
+        }
+    }
+}
